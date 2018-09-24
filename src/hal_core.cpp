9870226df3b79c22ddac085c6672dadf50dde9be
Lee Witek <neomech@gmail.com>
2018-09-24 20:56:36 +0100

    added keyboard input

---
+++ src/hal_core.cpp
@@ -0,0 +1,118 @@
+#include "hal_core.h"
+#include <iostream>
+#include <string>
+
+static SDL_Window* sdlWin = nullptr;
+static SDL_Renderer* sdlRen = nullptr;
+static SDL_Texture* sdlTex = nullptr;
+static SDL_PixelFormat* sdlPixFmt = nullptr;
+
+static void throw_error(std::string msg) {
+	msg += SDL_GetError();
+	throw(gfx_exception(msg));
+}
+
+void GFX_Init(int x, int y) {
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		throw_error("SDL_Init Error: ");
+	}
+
+	sdlWin = SDL_CreateWindow("thing!", 100, 100, x, y, SDL_WINDOW_SHOWN);
+	if (sdlWin == nullptr) {
+		throw_error("SDL_CreateWindow Error: ");
+	}
+
+	sdlRen = SDL_CreateRenderer(sdlWin, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (sdlRen == nullptr) {
+		throw_error("SDL_CreateRenderer Error: ");
+	}
+}
+
+void GFX_End() {
+	if (sdlRen) {
+		SDL_DestroyRenderer(sdlRen);
+	}
+	if (sdlWin) {
+		SDL_DestroyWindow(sdlWin);
+	}
+	if (sdlPixFmt) {
+		SDL_FreeFormat(sdlPixFmt);
+	}
+	if (sdlTex) {
+		SDL_DestroyTexture(sdlTex);
+	}
+	SDL_Quit();
+}
+
+void GFX_CreateBackBuffer(int x, int y) {
+	sdlTex = SDL_CreateTexture(sdlRen, SDL_PIXELFORMAT_RGB565, SDL_TEXTUREACCESS_STREAMING, x, y);
+	if (sdlTex == nullptr) {
+		throw_error("SDL_CreateTexture Error: ");
+	}
+
+	sdlPixFmt = SDL_AllocFormat(SDL_PIXELFORMAT_RGB565);
+}
+
+pixel_t GFX_GetPixel(uint8_t r, uint8_t g, uint8_t b) {
+	return (pixel_t)SDL_MapRGB(sdlPixFmt, r, g, b);
+}
+
+void GFX_LockBackBuffer(pixel_t** pixels, int* pitch) {
+	int res = SDL_LockTexture(sdlTex, NULL, (void**)pixels, pitch);
+	if (res < 0) {
+		throw_error("SDL_LockTexture Error: ");
+	}
+}
+
+void GFX_UnlockBackBuffer() {
+	SDL_UnlockTexture(sdlTex);
+}
+
+void GFX_Flip() {
+	SDL_RenderClear(sdlRen);
+	SDL_RenderCopy(sdlRen, sdlTex, NULL, NULL);
+	SDL_RenderPresent(sdlRen);
+}
+
+static uint8_t state = 0;
+
+void INP_ProcessInputEvents(const SDL_Event& ev) {
+	int mask = -1;
+	switch (ev.key.keysym.sym) {
+		case SDLK_LEFT:
+			mask = 1;
+			break;
+		case SDLK_RIGHT:
+			mask = 2;
+			break;
+		case SDLK_UP:
+			mask = 4;
+			break;
+		case SDLK_DOWN:
+			mask = 8;
+			break;
+		case SDLK_z:
+			mask = 16;
+			break;
+		case SDLK_x:
+			mask = 32;
+			break;
+		case SDLK_p:
+			mask = 64;
+			break;
+	}
+
+	if (mask != -1) {
+		if (ev.type == SDL_KEYDOWN) {
+			state = state | mask;
+		}
+		if (ev.type == SDL_KEYUP) {
+			state = state & (~mask);
+		}
+		std::cout << std::hex << (int)state << std::endl;
+	}
+}
+
+uint8_t INP_GetInputState() {
+	return state;
+}
---
+++ src/hal_core.h
@@ -0,0 +1,26 @@
+#ifndef GFX_CORE_H
+#define GFX_CORE_H
+
+#include <SDL2/SDL.h>
+#include <stdint.h>
+#include <stdexcept>
+
+typedef std::runtime_error gfx_exception;
+
+typedef uint16_t pixel_t;
+
+void GFX_Init(int x, int y);
+void GFX_End();
+
+pixel_t GFX_GetPixel(uint8_t r, uint8_t g, uint8_t b);
+
+void GFX_CreateBackBuffer(int x, int y);
+void GFX_LockBackBuffer(pixel_t** pixels, int* pitch);
+void GFX_UnlockBackBuffer();
+
+void GFX_Flip();
+
+void INP_ProcessInputEvents(const SDL_Event& ev);
+uint8_t INP_GetInputState();
+
+#endif /* GFX_CORE_H */
