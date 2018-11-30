611810c5012969053dc1fb03adc25b5a2928ed20
Lee Witek <neomech@gmail.com>
2018-11-30 01:57:02 +0000

    added decoding of music data

--- src/pico_audio.cpp
+++ src/pico_audio.cpp
@@ -1,5 +1,6 @@
 #include "pico_audio.h"
 #include "hal_audio.h"
+#include "pico_core.h"
 
 #include <sstream>
 
@@ -27,8 +28,25 @@ namespace pico_control {
 	void set_music_from_cart(std::string& data) {
 		std::istringstream str(data);
 		std::string line;
+		uint8_t* ptr = pico_control::get_music_data();
 		while (std::getline(str, line)) {
-			puts(line.c_str());
+			// puts(line.c_str());
+			int o[5] = {0};
+			if (sscanf(line.c_str(), "%02x %02x%02x%02x%02x", o, o + 1, o + 2, o + 3, o + 4) == 5) {
+				for (int n = 0; n < 4; n++) {
+					ptr[n] = o[n + 1];
+				}
+				if (o[0] & 1) {
+					ptr[0] |= 0x80;
+				}
+				if (o[0] & 2) {
+					ptr[1] |= 0x80;
+				}
+				if (o[0] & 4) {
+					ptr[2] |= 0x80;
+				}
+				ptr += 4;
+			}
 		}
 	}
 
@@ -36,7 +54,7 @@ namespace pico_control {
 		std::istringstream str(data);
 		std::string line;
 		while (std::getline(str, line)) {
-			puts(line.c_str());
+			// puts(line.c_str());
 		}
 	}
 
