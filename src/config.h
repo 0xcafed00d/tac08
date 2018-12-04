f04ece1dfc1b26406b2b2f112b0c5f29b6728eb7
Lee Witek <neomech@gmail.com>
2018-12-04 21:32:55 +0000

    fix freq typo in config.h

--- src/config.h
+++ src/config.h
@@ -5,7 +5,7 @@ namespace config {
 	const int SCREEN_WIDTH = 128;
 	const int SCREEN_HEIGHT = 128;
 	const int AUDIO_FREQ = 22050;
-	const int AUDIO_BUFFER_SIZE = 22050;
+	const int AUDIO_BUFFER_SIZE = 2048;
 	const int AUDIO_CHANNELS = 4;
 	const int PALETTE_SIZE = 16;
 }  // namespace config
