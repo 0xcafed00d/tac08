642125fbcd14eff61dc44fbe62540de5d0f7ee16
Lee Witek <neomech@gmail.com>
2018-12-19 14:42:27 +0000

    replace std::experimental::filesystem with custom functions

--- src/pico_cart.cpp
+++ src/pico_cart.cpp
@@ -1,4 +1,5 @@
-//#include <experimental/filesystem>
+#include <assert.h>
+#include <algorithm>
 #include <fstream>
 #include <map>
 #include <set>
@@ -12,7 +13,56 @@
 
 #include "utf8-util.h"
 
-// namespace fs = std::experimental::filesystem;
+namespace path {
+	std::string normalisePath(std::string path) {
+		std::replace(path.begin(), path.end(), '\\', '/');
+		return path;
+	}
+
+	std::string getPath(std::string path) {
+		std::size_t pos = path.find_last_of('/');
+		if (pos == std::string::npos) {
+			return "";
+		}
+		return path.substr(0, pos + 1);
+	}
+
+	std::string getFilename(std::string path) {
+		std::size_t pos = path.find_last_of('/');
+		if (pos == std::string::npos) {
+			return path;
+		}
+		return path.substr(pos + 1);
+	}
+
+	std::pair<std::string, std::string> splitFilename(std::string filename) {
+		std::size_t pos = filename.find_last_of('.');
+		if (pos == std::string::npos) {
+			return std::make_pair(filename, "");
+		}
+
+		return std::make_pair(filename.substr(0, pos), filename.substr(pos + 1));
+	}
+
+	void test() {
+		assert(normalisePath("/wibble\\test\\wibble/hello") == "/wibble/test/wibble/hello");
+
+		assert(getPath("/wibble/hatstand/file.ext") == "/wibble/hatstand/");
+		assert(getPath("file.ext") == "");
+		assert(getPath("/file.ext") == "/");
+		assert(getPath("/path/") == "/path/");
+
+		assert(getFilename("/path/") == "");
+		assert(getFilename("/path/name.txt") == "name.txt");
+		assert(getFilename("name.txt") == "name.txt");
+
+		typedef std::pair<std::string, std::string> pair;
+
+		assert(splitFilename("name.txt") == pair("name", "txt"));
+		assert(splitFilename("name") == pair("name", ""));
+		assert(splitFilename(".txt") == pair("", "txt"));
+	}
+}  // namespace path
 
 std::map<char32_t, uint8_t> emoji = {
     {0x2588, 0x80}, {0x2592, 0x81}, {0x1f431, 0x82}, {0x2b07, 0x83}, {0x2591, 0x84},
@@ -29,6 +79,8 @@ namespace pico_cart {
 	                                               "__sfx__", "__music__", "__label__"};
 
 	void do_load(std::istream& s, sections& sect) {
+		path::test();
+
 		std::string line;
 		std::string cur_sect = "header";
 
@@ -72,17 +124,16 @@ namespace pico_cart {
 	}
 
 	void load(std::string filename) {
+		filename = path::normalisePath(filename);
+
 		std::string data = FILE_LoadFile(filename);
 		if (data.size() == 0) {
 			throw error(std::string("failed to open cart file: ") + filename);
 		}
 
-		/*
-		        fs::path path(filename);
-		        cart["filename"] = filename;
-		        cart["base_path"] = path.remove_filename();
-		        cart["cart_name"] = path.filename().replace_extension("");
-		*/
+		cart["filename"] = filename;
+		cart["base_path"] = path::getPath(filename);
+		cart["cart_name"] = path::splitFilename(path::getFilename(filename)).first;
 
 		std::istringstream s(data);
 		do_load(s, cart);
